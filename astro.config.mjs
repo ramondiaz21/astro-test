ec38de9c65e02e582923e3486759344d09a8e0a8
Ramón Díaz <jramondiaz96@gmail.com>
2024-04-26 13:12:52 -0600

    blog posts and individuals posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,8 @@ export default defineConfig({
   experiments: {
     externalDependencies: true,
   },
+  routes: [
+    { path: "/", component: "./src/pages/index.astro" },
+    { path: "/:slug", component: "./src/pages/[slug].astro" },
+  ],
 });
