84587095a70455a2099ab31e46f0252487f23599
Ramón Díaz <jramondiaz96@gmail.com>
2024-04-25 22:16:09 -0600

    configuraciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
+import { defineConfig } from 'astro/config';
 
-import astroSass from "astro-sass";
-
-export default {
-  buildOptions: {
-    site: {
-      baseURL: "", // Puedes especificar la URL base de tu sitio aquí si es necesario
-    },
-  },
+// https://astro.build/config
+export default defineConfig({
   site: {
     hostname: "https://ramondiaz21.github.io", // Cambia esto a tu URL de GitHub Pages
     assets: "/astro-test/", // Cambia esto al nombre de tu repositorio en GitHub
   },
-};
+});
