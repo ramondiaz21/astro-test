455395928a84b5369088d70c6eb21b0eb3ed2d15
Ramón Díaz <jramondiaz96@gmail.com>
2024-04-25 22:17:36 -0600

    configuraciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-  site: {
-    hostname: "https://ramondiaz21.github.io", // Cambia esto a tu URL de GitHub Pages
-    assets: "/astro-test/", // Cambia esto al nombre de tu repositorio en GitHub
-  },
+  site: "https://ramondiaz21.github.io/astro-test/", // Cambia esto a la URL de tu sitio de GitHub Pages
 });
